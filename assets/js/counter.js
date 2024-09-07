3bcfc990e7485597f53c92a3bf8e7c2c45f22de4
Aastha Saxena <64476429+aasthasaxena217@users.noreply.github.com>
2024-09-07 10:05:13 +0530

    Update counter.js

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,5 +1,5 @@
 //countdown timer
-var countDownDate = new Date('November 4, 2023 9:00:00').getTime();
+var countDownDate = new Date('October 5th, 2024 8:00:00').getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
